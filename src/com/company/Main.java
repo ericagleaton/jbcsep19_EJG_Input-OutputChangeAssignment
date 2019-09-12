2512a0f2203ece6ae3d0d78c573b470b2965b20e
ericagleaton <EricaGleaton@comcast.net>
2019-09-12 09:41:35 -0400

    first push

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        double num1, num2, num3;
+
+        System.out.print("First temperature? ");
+        num1 = keyboard.nextDouble();
+
+        System.out.print("Second temperature? ");
+        num2 = keyboard.nextDouble();
+
+        System.out.println("The Average value is : " + ((num1+ num2)/2));
+    }
+}
